da653881da73ec59c3ca9c7d20452e7c3b75deca
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-21 13:14:58 -0400

    implemented bycrypt to encrypt andcompare password to signup and signin user

--- src/middleware/requireAuth.js
+++ src/middleware/requireAuth.js
@@ -4,7 +4,7 @@ const User = mongoose.model('User')
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers
-    console.log(req.headers)
+
     if(!authorization) {
         return res.status(401).send({error: 'You must be logged in '})
     }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,13 +7,11 @@ const config = require('../../config/config')
 const router = express.Router()
 
 router.post('/signup', async (request, response) => {
-    
+        
     const { email, password} = request.body
-    console.log(email, password)
     try {
-        const user = new User({email, password})
+        const user = new User({email, password})      
         await user.save()
-
         const token = jwt.sign({userId:user._id}, config.SECRET_KEY)
 
         response.send({
@@ -23,6 +21,33 @@ router.post('/signup', async (request, response) => {
         return response.status(422).send(error.message)
     }
    
+})
+
+
+router.post('/signin', async (request, response) => {
+
+    const { email, password } = request.body 
+
+    if(!email || !password) {
+        return response.status(422).send({error: 'Must provide email and password'})
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        return response.status(422).send({error: 'Invalid email or password'})
+    }
+
+    try {
+        const res = await user.comparePassword(password)
+        const token = jwt.sign({userId: user._id}, config.SECRET_KEY)    
+        response.send({
+            token
+        })
+    } catch(error) {
+        return response.status(422).send({error: 'Invalid email or password'})
+    }
+    
 })
 
 module.exports = router
